d64bb54bf98aceb7d5723aabb6cf806b37e0c987
Fang-Pen Lin <hello@fangpenlin.com>
2025-06-14 02:33:54 -0700

    Improve tests

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -11,13 +11,17 @@ from tinyloader.loader import Loader
 from tinyloader.loader import SharedMemoryShim
 
 
-class NormalLoader(Loader):
+class RandomLoader(Loader):
+    def __init__(self, data_size: tuple[int, ...], label_size: tuple[int, ...]):
+        self.data_size = data_size
+        self.label_size = label_size
+
     def make_request(self, item: float) -> typing.Any:
         return item
 
     def load(self, request: float) -> tuple[np.typing.NDArray, ...]:
-        return np.random.normal(request, 1.0, size=(3, 5)), np.random.normal(
-            request, 1.0, size=(4,)
+        return np.random.normal(request, 1.0, size=self.data_size), np.random.normal(
+            request, 1.0, size=self.label_size
         )
 
     def post_process(
@@ -30,14 +34,13 @@ class NormalLoader(Loader):
 
 
 def test_load():
-    loader = NormalLoader()
-
+    loader = RandomLoader(data_size=(64, 64), label_size=(4,))
     for x, y in load(loader, [1, 2, 3]):
         print(x.numpy(), y.numpy())
 
 
 def test_load_with_workers():
-    loader = NormalLoader()
+    loader = RandomLoader(data_size=(64, 64), label_size=(4,))
     with load_with_workers(loader, [1, 2, 3], 4) as generator:
         for x, y in generator:
             print(x.numpy(), y.numpy())
@@ -46,7 +49,9 @@ def test_load_with_workers():
 def test_share_memory_shim():
     multiprocessing.context.set_spawning_popen("spawn")
     with SharedMemoryManager() as smm:
-        loader = SharedMemoryShim(NormalLoader(), smm=smm)
+        loader = SharedMemoryShim(
+            RandomLoader(data_size=(64, 1024, 1024, 3), label_size=(4,)), smm=smm
+        )
         with load_with_workers(loader, range(1000), 4) as generator:
             for x, y in generator:
                 print(x.numpy(), y.numpy())
